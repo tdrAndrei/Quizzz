f51a71958e0aebb81d81c227fa7b6d5cb3479dc8
Juan José Tarazona Rodríguez <J.J.TarazonaRodriguez@student.tudelft.nl>
2022-02-24 18:39:25 +0100

    Added the user Entity

---
+++ commons/src/main/java/commons/User.java
@@ -0,0 +1,63 @@
+package commons;
+
+import javax.persistence.*;
+
+@Entity(name = "User")
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_seq",
+            sequenceName = "user_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_seq"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    User(){
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
