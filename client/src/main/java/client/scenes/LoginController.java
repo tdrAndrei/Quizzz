54c2845187e8a3146e0142099e0bf8a969799e96
Wiktor Grzybko <w.j.grzybko@student.tudelft.nl>
2022-02-28 21:02:02 +0100

    Creating loginController fields

--- client/src/main/java/client/scenes/LoginController.java
+++ client/src/main/java/client/scenes/LoginController.java
@@ -1,4 +1,42 @@
 package client.scenes;
 
+import client.utils.ServerUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javax.inject.Inject;
+
 public class LoginController {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @FXML
+    private TextField nameBox;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private TextField urlBox;
+
+    @Inject
+    public LoginController(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+    }
+
+    public void submit(){
+
+    }
+
+    public void quit(){
+
+    }
+
+
 }
