5f27b4b2f8d31ca89d9cbdc46f187b66772fe79c
Maarten van der Weide <m.v.vanderweide@student.tudelft.nl>
2022-03-02 12:45:51 +0100

    Fixed checkstyle violation

--- server/src/main/java/server/service/UserService.java
+++ server/src/main/java/server/service/UserService.java
@@ -2,8 +2,6 @@ package server.service;
 
 import commons.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import server.database.UserRepository;
 
