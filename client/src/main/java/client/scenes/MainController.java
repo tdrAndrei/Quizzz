bba67f156d1d2d271b6b76d18dfbdc33f87d521e
Wiktor Grzybko <w.j.grzybko@student.tudelft.nl>
2022-03-02 16:37:17 +0100

    Main menu scene created with the design

---
+++ client/src/main/java/client/scenes/MainController.java
@@ -0,0 +1,32 @@
+package client.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class MainController {
+
+    @FXML
+    private ImageView factImage;
+
+    @FXML
+    private Label factLabel;
+
+    @FXML
+    private Button leaderboardButton;
+
+    @FXML
+    private ImageView logoImage;
+
+    @FXML
+    private Button multiplayerButton;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private Button soloButton;
+
+}
+
