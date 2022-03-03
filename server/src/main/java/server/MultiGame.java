d526019a3f21d6340668355c2d1372b693fdece7
Juan José Tarazona Rodríguez <J.J.TarazonaRodriguez@student.tudelft.nl>
2022-03-03 17:57:23 +0100

    Added the barebones of the Player and Game classes

---
+++ server/src/main/java/server/MultiGame.java
@@ -0,0 +1,12 @@
+package server;
+
+import commons.Player;
+
+import java.util.List;
+
+public class MultiGame extends Game{
+
+    public MultiGame(List<Player> players, Long id) {
+        super(players, id);
+    }
+}
