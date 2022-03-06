c47f4190aed8063f1ffa4b3db220ec61f815b0d3
Juan José Tarazona Rodríguez <J.J.TarazonaRodriguez@student.tudelft.nl>
2022-03-06 16:10:24 +0100

    Fixed the checkstyle issues (hopefully)

--- server/src/test/java/server/service/LeaderboardEntryServiceTest.java
+++ server/src/test/java/server/service/LeaderboardEntryServiceTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LeaderboardEntryServiceTest {
     private TestLeaderboardEntryRepo repo;
--- server/src/test/java/server/service/TestLeaderboardEntryRepo.java
+++ server/src/test/java/server/service/TestLeaderboardEntryRepo.java
@@ -1,7 +1,6 @@
 package server.service;
 
 import commons.LeaderboardEntry;
-import commons.User;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
