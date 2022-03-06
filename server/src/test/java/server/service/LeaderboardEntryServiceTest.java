e0f03abad6fd3a52d80e5157ebcbfeb09b88e436
Juan José Tarazona Rodríguez <J.J.TarazonaRodriguez@student.tudelft.nl>
2022-03-06 16:01:32 +0100

    Tested leaderboard entry class and service

---
+++ server/src/test/java/server/service/LeaderboardEntryServiceTest.java
@@ -0,0 +1,106 @@
+package server.service;
+
+import commons.LeaderboardEntry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LeaderboardEntryServiceTest {
+    private TestLeaderboardEntryRepo repo;
+    private LeaderBoardEntryService sut;
+
+    @BeforeEach
+    public void setup() {
+        repo = new TestLeaderboardEntryRepo();
+        sut = new LeaderBoardEntryService(repo);
+    }
+
+    private static LeaderboardEntry getEntry(String name, int score) {
+        return new LeaderboardEntry(name, score);
+    }
+
+    @Test
+    public void cannotAddNullEntry(){
+        LeaderboardEntry entry = sut.insert(null);
+        assertNull(entry);
+    }
+
+    @Test
+    public void cannotAddNoNameEntry(){
+        LeaderboardEntry entry = sut.insert(getEntry("", 10));
+        assertNull(entry);
+    }
+
+
+    @Test
+    public void insertUsesDatabase(){
+        LeaderboardEntry entry = sut.insert(getEntry("test", 5));
+        assertTrue(repo.calledMethods.contains("save"));
+    }
+
+    @Test
+    public void insertSavesValidEntry(){
+        LeaderboardEntry entry = sut.insert(getEntry("test", 5));
+        assertEquals(entry.getName(), repo.entries.get(0).getName());
+    }
+
+    @Test
+    public void getAllEntriesUsesDatabase(){
+        List<LeaderboardEntry> entries = sut.getAllEntries();
+        assertTrue(repo.calledMethods.contains("findAll"));
+    }
+
+    @Test
+    public void getAllEntriesEmptyDB(){
+        List<LeaderboardEntry> entries = sut.getAllEntries();
+        assertEquals(entries, new ArrayList<LeaderboardEntry>());
+    }
+
+    @Test
+    public void getAllEntriesTwoEntries(){
+        sut.insert(getEntry("test1",5));
+        sut.insert(getEntry("test2", 10));
+        List<LeaderboardEntry> entries = sut.getAllEntries();
+        assertEquals(entries.get(0).getName(), "test1");
+        assertEquals(entries.get(0).getScore(), 5);
+        assertEquals(entries.get(1).getName(), "test2");
+        assertEquals(entries.get(1).getScore(), 10);
+    }
+
+    @Test
+    public void getByIdUsesDBWithInvalidId(){
+        sut.getById((long) 0);
+        assertTrue(repo.calledMethods.contains("existsById"));
+    }
+
+    @Test
+    public void getByIdUsesDBWithValidId(){
+        LeaderboardEntry entry = sut.insert(getEntry("testEntry", 5));
+        sut.getById((long) 1);
+        assertTrue(repo.calledMethods.contains("existsById"));
+        assertTrue(repo.calledMethods.contains("findById"));
+    }
+
+    @Test
+    public void getByIdFindsExistingEntry(){
+        LeaderboardEntry entry = sut.insert(getEntry("testEntry", 5));
+        Optional<LeaderboardEntry> optionalEntry = sut.getById((long) 1);
+        assertTrue(optionalEntry.isPresent());
+        assertEquals(optionalEntry.get().getName(), entry.getName());
+        assertEquals(optionalEntry.get().getScore(), entry.getScore());
+    }
+
+    @Test
+    public void getByIdFindsNonExistingEntry(){
+        Optional<LeaderboardEntry> optionalEntry = sut.getById((long) 1);
+        assertFalse(optionalEntry.isPresent());
+    }
+}
---
+++ server/src/test/java/server/service/TestLeaderboardEntryRepo.java
@@ -0,0 +1,187 @@
+package server.service;
+
+import commons.LeaderboardEntry;
+import commons.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import server.database.LeaderboardEntryRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class TestLeaderboardEntryRepo implements LeaderboardEntryRepository {
+
+    private int id = 1;
+    public final List<LeaderboardEntry> entries = new ArrayList<>();
+    public final List<String> calledMethods = new ArrayList<>();
+
+    private void call(String name) {
+        calledMethods.add(name);
+    }
+
+    @Override
+    public List<LeaderboardEntry> findAll() {
+        call("findAll");
+        return entries;
+    }
+
+    @Override
+    public List<LeaderboardEntry> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<LeaderboardEntry> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<LeaderboardEntry> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(LeaderboardEntry entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends LeaderboardEntry> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> S save(S entity) {
+        call("save");
+        entity.id = (long) id++;
+        entries.add(entity);
+        return entity;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<LeaderboardEntry> findById(Long aLong) {
+        call("findById");
+        for (LeaderboardEntry e : entries){
+            if (e.getId().equals(aLong)){
+                return Optional.of(e);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        call("existsById");
+        for (LeaderboardEntry e : entries){
+            if (e.getId().equals(aLong)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<LeaderboardEntry> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public LeaderboardEntry getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public LeaderboardEntry getById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends LeaderboardEntry, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+}
