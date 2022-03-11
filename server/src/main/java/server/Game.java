0b0f37346b6e02615eee375d9e930a61ae59472a
Maarten van der Weide <m.v.vanderweide@student.tudelft.nl>
2022-03-11 14:24:03 +0100

    Changed Long -> long where possible

--- server/src/main/java/server/Game.java
+++ server/src/main/java/server/Game.java
@@ -16,7 +16,7 @@ import java.util.*;
 public class Game {
 
     private Question currentQuestion;
-    private final Long id;
+    private final long id;
     private final QuestionService questionService;
     private final Map<Long, Player> playerMap = new HashMap<>();
     //private int playerLimit;
@@ -26,20 +26,21 @@ public class Game {
     private final Map<Long, Message> diffMap = new HashMap<>();
     private int amountAnswered = 0;
 
-    public Game(Long id, QuestionService questionService) {
+    public Game(long id, QuestionService questionService) {
         this.id = id;
         this.questionService = questionService;
         stageQueue.add(new MutablePair<>("Waiting", Integer.MAX_VALUE));
         for (int i = 0; i < 10; i++) {
-            stageQueue.add(new MutablePair<>("Question", 20));
+            stageQueue.add(new MutablePair<>("Question", 7));
             stageQueue.add(new MutablePair<>("CorrectAns", 3));
         }
         stageQueue.add(new MutablePair<>("Leaderboard", 7));
         for (int i = 0; i < 10; i++) {
-            stageQueue.add(new MutablePair<>("Estimate", 20));
+            stageQueue.add(new MutablePair<>("Estimate", 7));
             stageQueue.add(new MutablePair<>("CorrectAns", 3));
         }
         stageQueue.add(new MutablePair<>("End", 15));
+
         initializeStage();
     }
 
@@ -52,10 +53,10 @@ public class Game {
         insertMessageIntoDiff(new NewPlayersMessage("NewPlayers", new ArrayList<>(playerMap.values())));
     }
 
-    public void initializeStage(){
+    public void initializeStage() {
         amountAnswered = 0;
         Pair<String, Integer> stagePair = stageQueue.peek();
-        if (stagePair == null){
+        if (stagePair == null) {
             return;
         }
         String stage = stagePair.getKey();
@@ -97,33 +98,34 @@ public class Game {
         }
     }
 
-    public void processAnswer(Long userAnswer, Long userId){
+    public void processAnswer(long userAnswer, long userId) {
         int elapsed = ((int) (new Date().getTime() - startTime.getTime()) / 1000);
         Player currPlayer = playerMap.get(userId);
         currPlayer.setScore(currPlayer.getScore() + currentQuestion.calculateScore(userAnswer, elapsed));
         amountAnswered++;
     }
 
-    public Message getUpdate(Long userId){
+    public Message getUpdate(long userId) {
         Message update = diffMap.get(userId);
         diffMap.put(userId, new NullMessage("None"));
         return update;
     }
 
-    public void removePlayer(Long userId) {
+    public void removePlayer(long userId) {
         playerMap.remove(userId);
         diffMap.remove(userId);
         maxTime.remove(userId);
 
+        //If we are still in waiting screen, inform clients of updated player list
         Pair<String, Integer> pair = stageQueue.peek();
         if (pair != null && pair.getKey().equals("Waiting")) {
             insertMessageIntoDiff(new NewPlayersMessage("NewPlayers", new ArrayList<>(playerMap.values())));
         }
     }
 
-    public void halfTimeJoker(Long jokerUserId){
-        for (Long otherPlayerId : playerMap.keySet()){
-            if (otherPlayerId.equals(jokerUserId)) {
+    public void halfTimeJoker(long jokerUserId) {
+        for (long otherPlayerId : playerMap.keySet()) {
+            if (otherPlayerId == (jokerUserId)) {
                 continue;
             }
             maxTime.put(otherPlayerId, maxTime.get(otherPlayerId) / 2);
@@ -132,12 +134,13 @@ public class Game {
 
     public void ifStageEnded() {
         Date date = new Date();
-        if (amountAnswered == playerMap.size()){
+        if (amountAnswered == playerMap.size()) {
             stageQueue.poll();
             initializeStage();
+            return;
         }
 
-        for (int maxTime : maxTime.values()){
+        for (int maxTime : maxTime.values()) {
             if (!((date.getTime() - startTime.getTime()) / 1000 > maxTime)) {
                 break;
             }
@@ -146,23 +149,23 @@ public class Game {
         }
     }
 
-    public void insertCorrectAnswerIntoDiff(){
-        for (Long id : diffMap.keySet()){
+    public void insertCorrectAnswerIntoDiff() {
+        for (long id : diffMap.keySet()) {
             CorrectAnswerMessage answerMessage = new CorrectAnswerMessage("ShowCorrectAnswer", playerMap.get(id).getScore(), currentQuestion.getAnswer());
             diffMap.put(id, answerMessage);
         }
 
     }
-    public void insertMessageIntoDiff(Message message){
+    public void insertMessageIntoDiff(Message message) {
         diffMap.replaceAll((i, v) -> message);
     }
-    public void insertQuestionIntoDiff(Question question){
-        for (Long id : diffMap.keySet()){
+    public void insertQuestionIntoDiff(Question question) {
+        for (long id : diffMap.keySet()) {
             NewQuestionMessage questionMessage = new NewQuestionMessage("NewQuestion", question, playerMap.get(id).getScore());
             diffMap.put(id, questionMessage);
         }
     }
-    public void setMaxTime(Integer time){
+    public void setMaxTime(Integer time) {
         maxTime.replaceAll((i, v) -> time);
     }
 
@@ -174,7 +177,7 @@ public class Game {
         this.currentQuestion = currentQuestion;
     }
 
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
