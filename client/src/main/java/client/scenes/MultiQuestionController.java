c5f827877ffd2e7308bb5b0f4eeffa9925ec6827
Maarten van der Weide <m.v.vanderweide@student.tudelft.nl>
2022-03-23 11:38:46 +0100

    Moved border to AnchorPane
    - Moved green / orange border to anchorpane
    - Gave orange border to user selected answer

--- client/src/main/java/client/scenes/MultiQuestionController.java
+++ client/src/main/java/client/scenes/MultiQuestionController.java
@@ -45,13 +45,13 @@ public class MultiQuestionController implements Initializable {
     private HBox jokerContainer;
 
     @FXML
-    private GridPane Answer1;
+    private AnchorPane ans1pane;
 
     @FXML
-    private GridPane Answer2;
+    private AnchorPane ans2pane;
 
     @FXML
-    private GridPane Answer3;
+    private AnchorPane ans3pane;
 
     @FXML
     private Button exitButton;
@@ -92,7 +92,8 @@ public class MultiQuestionController implements Initializable {
     private double baseWidth;
     private double baseHeight;
     private long chosenAnswer;
-    private Border correctAnswerBorder = new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5)));
+    private final Border correctAnswerBorder = new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5)));
+    private final Border selectedAnswerBorder = new Border(new BorderStroke(Color.ORANGE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5)));
 
     private MainCtrl mainCtrl;
     private final ClientGameController clientGameController;
@@ -143,13 +144,13 @@ public class MultiQuestionController implements Initializable {
         long index = message.getCorrectAnswer();
         String answer = "";
         if (index == 0) {
-            Answer1.setBorder(correctAnswerBorder);
+            ans1pane.setBorder(correctAnswerBorder);
             activity1Label.setTextFill(Color.rgb(0, 225, 0));
         } else if (index == 1) {
-            Answer2.setBorder(correctAnswerBorder);
+            ans2pane.setBorder(correctAnswerBorder);
             activity2Label.setTextFill(Color.rgb(0, 225, 0));
         } else {
-            Answer3.setBorder(correctAnswerBorder);
+            ans3pane.setBorder(correctAnswerBorder);
             activity3Label.setTextFill(Color.rgb(0, 225, 0));
         }
         if (chosenAnswer == index) {
@@ -186,7 +187,9 @@ public class MultiQuestionController implements Initializable {
                     node.setScaleY(1);
                 });
             }
-
+            ans1pane.setStyle("-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.3), 10, 0.5, 0.0, 0.0);");
+            ans2pane.setStyle("-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.3), 10, 0.5, 0.0, 0.0);");
+            ans3pane.setStyle("-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.3), 10, 0.5, 0.0, 0.0);");
             progressBar.setProgress(1.0);
 
             Timer timer = new Timer();
@@ -236,11 +239,13 @@ public class MultiQuestionController implements Initializable {
             }
         }
 
+
         public void submit1(){
             if (!clientGameController.isDisableJokerUsage()) {
                 chosenAnswer = 0;
                 clientGameController.submitAnswer(0);
                 clientGameController.setDisableJokerUsage(true);
+                ans1pane.setBorder(selectedAnswerBorder);
             }
         }
         public void submit2(){
@@ -248,6 +253,7 @@ public class MultiQuestionController implements Initializable {
                 chosenAnswer = 1;
                 clientGameController.submitAnswer(1);
                 clientGameController.setDisableJokerUsage(true);
+                ans2pane.setBorder(selectedAnswerBorder);
             }
         }
         public void submit3(){
@@ -255,6 +261,7 @@ public class MultiQuestionController implements Initializable {
                 chosenAnswer = 2;
                 clientGameController.submitAnswer(2);
                 clientGameController.setDisableJokerUsage(true);
+                ans3pane.setBorder(selectedAnswerBorder);
             }
         }
 
@@ -275,9 +282,9 @@ public class MultiQuestionController implements Initializable {
             activity1Label.setTextFill(Color.rgb(0 , 0 , 0));
             activity2Label.setTextFill(Color.rgb(0 , 0 , 0));
             activity3Label.setTextFill(Color.rgb(0 , 0 , 0));
-            Answer1.setBorder(Border.EMPTY);
-            Answer2.setBorder(Border.EMPTY);
-            Answer3.setBorder(Border.EMPTY);
+            ans1pane.setBorder(Border.EMPTY);
+            ans2pane.setBorder(Border.EMPTY);
+            ans3pane.setBorder(Border.EMPTY);
         }
 
         public void useDoublePointsJoker() {
